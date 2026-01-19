9e07f7fca719b0606c907cd0ad12b026c4cf1d68
Saramsh Gautam <saramshgautam@gmail.com>
2026-01-19 11:14:40 -0600

    Set up for TeamC

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -155,7 +155,7 @@ const LoginPage = () => {
         role: userData.role || "participant",
         studyId: userData.studyId || "Eval3333",
         taskName: userData.taskName || "ConditionC2",
-        teamId: userData.teamId || "TeamG",
+        teamId: userData.teamId || "TeamC",
         createdAt: serverTimestamp(),
       });
 
@@ -163,7 +163,7 @@ const LoginPage = () => {
 
       const studyId = userData.studyId || "Eval3333";
       const taskName = userData.taskName || "ConditionC2";
-      const teamId = userData.teamId || "TeamG";
+      const teamId = userData.teamId || "TeamC";
 
       navigate(
         `/whiteboard/${encodeURIComponent(studyId)}/${encodeURIComponent(
